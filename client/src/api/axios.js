ffddc7864b5205b9476c6d04da524a9ef6a2e8d2
angelsanchez-dev <angelsanchez0113@gmail.com>
2026-04-27 23:40:43 -0300

    Fix: URL de producción final

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api'
+  baseURL: 'https://centro-raices-fullstack.onrender.com/api'
 });
 
 // Interceptor para pegar el token en el Header
